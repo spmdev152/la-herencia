d19bedda843f16452469fcbc95166772a33dee7d
carlosSerra <cserra9948@gmail.com>
2025-06-05 20:28:55 +0200

    feat: mobile responsive

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ function Menu() {
     <section className="menu-section">
       <div className="menu-layout">
         <div className="menu-title-vertical">MENÚ</div>
-        <div>
+        <div className="menu-content-blocks">
           <div className="menu-alma-block alma-relative">
             <h3>ALMA</h3>
             <ul>
@@ -26,22 +26,22 @@ function Menu() {
             </ul>
             <h4>POSTRE</h4>
             <p>Delicatesen de nuestro maestro pastelero Martín Rivera</p>
-            <div className="menu-nanci-block">
-              <h3>NANCI 25</h3>
-              <ul>
-                <li>Salazón</li>
-                <li>Encurtidos</li>
-                <li>Papas Parlamento</li>
-                <li>Rebujitos</li>
-                <li>Croft Soda/ Tío Pepe</li>
-                <li>Licor 43</li>
-                <li>Ginger</li>
-              </ul>
-            </div>
-            <a href="/ruta/menu.pdf" target="_blank" rel="noopener noreferrer" className="menu-download-cta">
-              <img src="/buttons/DescargarMenu.png" alt="Descargar menú del evento" />
-            </a>
           </div>
+          <div className="menu-nanci-block">
+            <h3>NANCI 25</h3>
+            <ul>
+              <li>Salazón</li>
+              <li>Encurtidos</li>
+              <li>Papas Parlamento</li>
+              <li>Rebujitos</li>
+              <li>Croft Soda/ Tío Pepe</li>
+              <li>Licor 43</li>
+              <li>Ginger</li>
+            </ul>
+          </div>
+          <a href="/ruta/menu.pdf" target="_blank" rel="noopener noreferrer">
+            <img src="/buttons/DescargarMenu.png"className='cta-img' alt="Descargar menú del evento" />
+          </a>
           <div className="menu-extra-text">
             Además del menú, habrá más sorpresas a lo largo del evento.<br />
             Y, por supuesto, para los más fiesteros, contaremos con snacks fríos para reponer fuerzas y seguir bailando.
